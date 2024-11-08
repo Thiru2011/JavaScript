4881ed6d742603f3bdd869135835dc3a570d9479
Thiru2011 <vinoappu201@gmail.com>
2024-11-08 17:55:37 +0530

    add file

---
+++ script/script.js
@@ -0,0 +1,6 @@
+
+var myVariable = "aginai";
+document.write(myVariable);
+document.write(myVariable.charAt(2))
+document.write(myVariable.lastIndexOf("i"));
+document.write(myVariable.charAt("0"));
